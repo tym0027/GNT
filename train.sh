a40320a1b9e9cf1059aa2e29d7d54771a6c32d3b
tym0027 <tarupprecht@gmail.com>
2022-08-19 16:33:45 -0400

    adding important log files, scripts, and onnx export file to repo

---
+++ train.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=2-00:00:00
+#SBATCH --job-name=truppr20220811
+#SBATCH --partition=ce-mri
+#SBATCH --gres=gpu:v100:2
+#SBATCH --cpus-per-task=48
+#SBATCH --output=%j.log
+
+# python3 train.py --config configs/gnt_blender.txt --train_scenes chair --eval_scenes chair
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.conda/envs/gnt/lib/
+python3 train.py --config configs/gnt_blender.txt --train_scenes chair --eval_scenes chair
+
